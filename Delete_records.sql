acbe4b5985175087a2ca1bd50b17b1778fcc0407
teodoradruta <46301305+teodoradruta@users.noreply.github.com>
2019-01-19 00:16:41 +0200

    Add files via upload

---
+++ Delete_records.sql
@@ -0,0 +1,41 @@
+﻿DELETE FROM Accounts
+DELETE FROM DiaryEntry
+DELETE FROM Diary
+DELETE FROM Goals
+DELETE FROM MyMeals
+DELETE FROM MealGoals
+DELETE FROM NutritionGoals
+
+DELETE FROM AccountCredentials
+DELETE FROM  Accounts_Cardio
+DELETE FROM Accounts_Friends
+DELETE FROM  Accounts_Meals
+DELETE FROM Accounts_Notification
+DELETE FROM Accounts_Recipes
+DELETE FROM Accounts_Reminder
+DELETE FROM Accounts_Strength
+DELETE FROM BreakfastGoals
+DELETE FROM DiaryBreakfast
+DELETE FROM DiaryCardio
+DELETE FROM DiaryDinner
+DELETE FROM DiaryLunch
+DELETE FROM DiaryNotes
+DELETE FROM DiarySnack1
+DELETE FROM DiarySnack2
+DELETE FROM DiaryStrength
+DELETE FROM DiaryWater
+DELETE FROM FitnessGoals
+DELETE FROM WeightGoals
+DELETE FROM Snack1Goals
+DELETE FROM Snack2Goals
+DELETE FROM LunchGoals
+DELETE FROM DinnerGoals
+
+
+DELETE FROM MyCardio
+DELETE FROM MyRecipes
+DELETE FROM MyStrength
+DELETE FROM Notifications
+DELETE FROM Posts
+DELETE FROM Progress
+
